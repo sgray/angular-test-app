2e6f05c3454572defa1f1b5b9fc3de2b0a87492e
Steve Gray <swgray11@gmail.com>
2020-04-30 05:31:02 -0500

    Add call to setTimeout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnInit {
+  blurTimeout: number;
   title = 'test-app';
+
+  ngOnInit() {
+    this.blurTimeout = setTimeout(() => { console.log('test'); });
+  }
+
+  ngAfterViewInit() {
+    console.log(this.blurTimeout);
+  }
 }
